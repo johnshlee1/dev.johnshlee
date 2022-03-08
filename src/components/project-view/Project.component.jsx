7498c384fac14e7a4d6b9bfdf02d7763a8d8e0fd
John Lee <john.lee@deliveryhero.com>
2022-03-08 02:49:21 +0100

    added arrow and embeded external project link to image as well:

--- src/components/project-view/Project.component.jsx
+++ src/components/project-view/Project.component.jsx
@@ -9,7 +9,9 @@ export const ProjectComponent = ({
 }) => (
   <div className="project-container">
     <div className="project-image-container">
-      <img className="project-image" alt="project" src={imageUrl} />
+      <a href={projectUrl} target="_blank" rel="noreferrer">
+        <img className="project-image" alt="project" src={imageUrl} />
+      </a>
     </div>
     <div className="project-text-container">
       <div className="project-title-container">
@@ -19,8 +21,18 @@ export const ProjectComponent = ({
           target="_blank"
           rel="noreferrer"
         >
-          <p className="project-title">{projectTitle}</p>
-          <i className="fa fa-arrow"></i>
+          <p className="project-title">{projectTitle} </p>
+          <svg
+            className="north-east-arrow"
+            xmlns="http://www.w3.org/2000/svg"
+            enable-background="new 0 0 24 24"
+            // height="24"
+            viewBox="0 0 24 24"
+            // width="24"
+          >
+            <rect fill="none" height="24" width="24" />
+            <path d="M9,5v2h6.59L4,18.59L5.41,20L17,8.41V15h2V5H9z" />
+          </svg>
         </a>
       </div>
       <div className="project-description">
